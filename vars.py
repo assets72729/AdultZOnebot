d276248d7a63511fcc3f6a36475d70067484004f
assets72729 <hunxutia@gmail.com>
2025-08-05 14:46:05 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,17 +1,17 @@
 import os
 from typing import List
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7624888045:AAFIS3GtRHXy")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://xxxxx:mkooaa@werdeveloper.vxfam.mongodb.net/")
-DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-10026550xxxx"))
-ADMIN_ID = int(os.getenv("ADMIN_ID", "xxxxxxxx"))
-LOG_CHNL = int(os.getenv("LOG_CHNL", "-1002357280xxx"))
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "") # Without @
+API_ID = int(os.getenv("API_ID", "24890303"))
+API_HASH = os.getenv("API_HASH", "94cf78d1e6883ecb10f32e31fc23cfe0")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URI", "")
+DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1002723839767"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7289855833"))
+LOG_CHNL = int(os.getenv("LOG_CHNL", "-1002890514174"))
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "HiiWlcme") # Without @
 IS_FSUB = bool(os.environ.get("FSUB", True))
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002528152xxx").split()))
-DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", "-1002652997xxx"))
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002705791364").split()))
+DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", "-1002890514174"))
 FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "240"))
 
 
