7f6559873fe2b2137fe5f3183e8502fbfdd344be
Dypixx <itsdypixx@gmail.com>
2025-08-05 12:05:00 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from bot import bot
+from pyrogram import idle
+
+async def start():
+    await bot.start()
+
+bot.loop.create_task(start())
+idle()
+
+'''
+Smile, please... 😊
+Goodbye forever.
+
+Thank you all for your care and support throughout this journey.
+
+Unfortunately, my @dypixx account has been hacked. The hacker is now using it to promote unknown content. Please be cautious, stay alert, and don’t fall for anything they post.
+
+Take care, stay safe — and once again, thank you for everything.
+Goodbye.
+
+— Dypixx
+'''
