bee777357fe82fd7f3802827951f9df4d21382d5
Dypixx <itsdypixx@gmail.com>
2025-08-05 12:05:07 +0530

    Added vars.py

---
+++ vars.py
@@ -0,0 +1,30 @@
+import os
+from typing import List
+
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7624888045:AAFIS3GtRHXy")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://xxxxx:mkooaa@werdeveloper.vxfam.mongodb.net/")
+DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-10026550xxxx"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "xxxxxxxx"))
+LOG_CHNL = int(os.getenv("LOG_CHNL", "-1002357280xxx"))
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "") # Without @
+IS_FSUB = bool(os.environ.get("FSUB", True))
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002528152xxx").split()))
+DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", "-1002652997xxx"))
+FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "240"))
+
+
+'''
+Smile, please... 😊
+Goodbye forever.
+
+Thank you all for your care and support throughout this journey.
+
+Unfortunately, my @dypixx account has been hacked. The hacker is now using it to promote unknown content. Please be cautious, stay alert, and don’t fall for anything they post.
+
+Take care, stay safe — and once again, thank you for everything.
+Goodbye.
+
+— Dypixx
+'''
