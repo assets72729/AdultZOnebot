b5739a4a7f70e2814fb0f2aa25c4b70c33bd4a09
Dypixx <itsdypixx@gmail.com>
2025-08-05 12:05:28 +0530

    Added Dypixx/Extraa/index.py

---
+++ Dypixx/Extraa/index.py
@@ -0,0 +1,34 @@
+'''
+Smile, please... 😊
+Goodbye forever.
+
+Thank you all for your care and support throughout this journey.
+
+Unfortunately, my @dypixx account has been hacked. The hacker is now using it to promote unknown content. Please be cautious, stay alert, and don’t fall for anything they post.
+
+Take care, stay safe — and once again, thank you for everything.
+Goodbye.
+
+— Dypixx
+'''
+
+from pyrogram import Client, filters
+from vars import *
+from Database.maindb import mdb
+from pyrogram.types import Message
+from pyrogram.types import *
+
+@Client.on_message(filters.chat(DATABASE_CHANNEL_ID) & filters.video)
+async def save_video(client: Client, message: Message):
+    try:
+        video_id = message.id
+        video_duration = message.video.duration
+        is_premium = video_duration > FREE_VIDEO_DURATION
+        await mdb.save_video_id(video_id, video_duration, is_premium)
+        text = f"**✅ Saved | ID: {video_id} | ⏱️ {video_duration}s | 💎 {is_premium}**"
+        await client.send_message(chat_id=DATABASE_CHANNEL_LOG, text=text)
+    except Exception as t:
+        print(f"Error: {str(t)}")
+
+
+        
