52618786a4f9e45ddf7dad23b8299e1d9189a93d
assets72729 <hunxutia@gmail.com>
2025-08-05 20:54:20 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,16 +1,16 @@
 import os
 from typing import List
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
+API_ID = int(os.getenv("API_ID", "24890303"))
+API_HASH = os.getenv("API_HASH", "94cf78d1e6883ecb10f32e31fc23cfe0")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 MONGO_URI = os.getenv("MONGO_URI", "")
-DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", ""))
-ADMIN_ID = int(os.getenv("ADMIN_ID", ""))
-LOG_CHNL = int(os.getenv("LOG_CHNL", ""))
+DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1002899668085"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7289855833"))
+LOG_CHNL = int(os.getenv("LOG_CHNL", "-1002799922811"))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "HiiWlcme") # Without @
 IS_FSUB = bool(os.environ.get("FSUB", True))
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "").split()))
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002705791364").split()))
 DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", ""))
 FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "240"))
 
@@ -29,3 +29,4 @@ Goodbye.
 — Dypixx
 
 '''
+
